7c194afa12118596c2d39530a0690e9723b19450
VivianeAntonio <asilvaviviane@gmail.com>
2022-01-25 17:43:54 -0300

    implementando states

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import appConfig from '../config.json';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
+import { useRouter } from 'next/router';
+import React from 'react';
 
 
 function Titulo(props) {
@@ -61,11 +34,12 @@ function Titulo(props) {
 //  export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'VivianeAntonio';
-
+    //const username = 'VivianeAntonio';
+    const [username, setUsername]= React.useState('VivianeAntonio');
+    const roteamento = useRouter();
+    
     return (
-        <>
-            <GlobalStyle />
+        <>            
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -92,6 +66,12 @@ export default function PaginaInicial() {
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento){
+                            infosDoEvento.preventDefault();
+                            console.log('Alguem submeteu o form');
+                            roteamento.push('/chat');
+                            //window.location.href = '/chat';
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,7 +82,28 @@ export default function PaginaInicial() {
                             {appConfig.name}
                         </Text>
 
+                        {/* <input 
+                            type="text"
+                            value={username}
+                            onChange={function (event) {
+                                console.log("usuario digitou", event.target.value)
+                                // Onde ta o valor?
+                                const valor = event.target.value;
+                                // Trocar o valor da variável
+                                // atraves do React e avise quem precisa
+                                setUsername(valor);
+                            }}                            
+                        /> */}
                         <TextField
+                            value={username}
+                            onChange={function (event) {
+                                console.log("usuario digitou", event.target.value)
+                                // Onde ta o valor?
+                                const valor = event.target.value;
+                                // Trocar o valor da variável
+                                // atraves do React e avise quem precisa
+                                setUsername(valor);
+                            }} 
                             fullWidth
                             textFieldColors={{
                                 neutral: {
